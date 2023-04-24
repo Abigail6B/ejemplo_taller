9b1e1f15bc28876284ac2eb1c59f613642a2784f
abigail santamaria barragan <utp0145723@alumno.utpuebla.edu.mx>
2023-04-24 10:17:53 -0600

    Creacion de controlador taller

---
+++ application/views/Taller/index_view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista por defecto</title>
+</head>
+<body>
+    <h1>Controlador: Taller</h1>
+    <h2>Taller: Vista por defecto</h2>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit officia tempore ipsam cumque velit saepe deleniti quam dignissimos facere ratione perferendis cum minus ipsum, odio veritatis quibusdam magni laudantium libero.</p>
+    </div>
+
+</body>
+</html>
---
+++ application/views/primer_vista_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola</h1>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum natus aliquid corrupti voluptatum, doloremque eius, quisquam incidunt corporis ex aperiam, perspiciatis est aliquam provident rem rerum vero quidem ipsum nisi.
+
+    </p>
+</body>
+</html>
