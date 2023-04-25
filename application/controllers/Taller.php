07c084dede0d57b1ba33cfe9908bc69583153b19
abigail santamaria barragan <utp0145723@alumno.utpuebla.edu.mx>
2023-04-25 10:16:46 -0600

    Actualizaciones con Templete

--- application/controllers/Taller.php
+++ application/controllers/Taller.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Taller extends CI_Controller {
 
+	//creacion de un construtor
+	public function __construct(){
+		//Hereda los metodos de la super clase CI_Controles
+		parent:: __construct();
+		//Agregar Helpers y Librerias adicionales al controlador
+		$this->load->helper('url');
+	}
+
 	public function index()
 	{
 		$this->load->view('Taller/index_view');
@@ -16,5 +24,10 @@ class Taller extends CI_Controller {
     public function agregar_taller(){
 		//llamar un metodo que carge la vista
 		$this->load->view('Taller/agregar_taller_view');
+	}
+
+	public function blank_view(){
+		//llamar un metodo que carge la vista
+		$this->load->view('Taller/blank_view');
 	}
 }
